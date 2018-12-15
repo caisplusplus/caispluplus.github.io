19389bb0e7edbf551ffe4e6e37351c3c8ff7b769
Sean Syed <seansyed@usc.edu>
2018-12-15 01:17:18 -0800

    remove log messages

--- assets/js/menuOpacity.js
+++ assets/js/menuOpacity.js
@@ -1,15 +1,21 @@
-var t1 = 480;
+var t1 = 464;
 var d1 = 146;
+var t2 = 4468;
+var d2 = 146;
 
 $(window).scroll(function() {
     var scroll = $(document).scrollTop();
-    
+    console.log("Scroll: " + scroll);
     if (scroll > t1 && scroll < t1 + d1) {
         var newOpac = .75 + (((scroll - t1) / d1) * .25);
         $("#header").css({'opacity': newOpac});
-        
-    } else if (scroll > t1) {
+        console.log("Opacity: " + newOpac);
+    } else if (scroll > t1 + d1 && scroll < t2) {
         $("#header").css({'opacity': 1});
+    } else if (scroll > t2) {
+        var newOpac = 1 - (((scroll - t2) / d2) * .25);
+        $("#header").css({'opacity': newOpac});
+        console.log('Opacity: ' + newOpac);
     } else {
         $("#header").css({'opacity': 0.75});
     }
