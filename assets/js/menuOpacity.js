d0a981a433a09914886236d56dc1952dbc443865
Sean Syed <seansyed@usc.edu>
2018-12-18 16:01:48 -0800

    remove console.log

--- assets/js/menuOpacity.js
+++ assets/js/menuOpacity.js
@@ -5,17 +5,14 @@ var d2 = 146;
 
 $(window).scroll(function() {
     var scroll = $(document).scrollTop();
-    console.log("Scroll: " + scroll);
     if (scroll > t1 && scroll < t1 + d1) {
         var newOpac = .75 + (((scroll - t1) / d1) * .25);
         $("#header").css({'opacity': newOpac});
-        console.log("Opacity: " + newOpac);
     } else if (scroll > t1 + d1 && scroll < t2) {
         $("#header").css({'opacity': 1});
     } else if (scroll > t2) {
         var newOpac = 1 - (((scroll - t2) / d2) * .25);
         $("#header").css({'opacity': newOpac});
-        console.log('Opacity: ' + newOpac);
     } else {
         $("#header").css({'opacity': 0.75});
     }
